9a2a04a6c7ae059c08898ecdc736b8197d3b09ae
Ramyking <raminmehrabi@Ramins-MacBook-Air.local>
2016-12-09 23:05:55 +0330

    SCSS

---
+++ extend-scss/js/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Pan = React.createClass({
+  displayName: "Pan",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "div",
+        { className: "little-panel" },
+        "little panel"
+      ),
+      React.createElement(
+        "div",
+        { className: "big-panel" },
+        "big panel"
+      )
+    );
+  }
+});
+ReactDOM.render(React.createElement(Pan, null), document.getElementById("container"));
